7cf597adbef17cb62426fcd765112231a237ac71
Tobias Baumgartner <tobias.baumgartner@ag.ch>
2024-03-14 23:05:35 +0100

    Grid Funktionalität

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,17 @@ function App() {
 
   return (
     <>
-      <SidebarElement title="Migros-Vereinsbons"></SidebarElement>
-      <SidebarElement title="NEWS"></SidebarElement>
-      <SidebarElement title="Corona-Schutzkonzept"></SidebarElement>
-
-      <br></br>
-      <p style={{ color: "red" }}>So styled man CSS Inline.</p>
-      <p className="laila">So styled man CSS mit einer CSS Klasse aus einem anderen File zum Beispiel.</p>
+    <div className="grid">
+      <div className="main">
+        <p style={{ color: "red" }}>So styled man CSS Inline.</p>
+        <p className="laila">So styled man CSS mit einer CSS Klasse aus einem anderen File zum Beispiel.</p>
+      </div>
+      <div className="sidebar">
+        <SidebarElement title="Migros-Vereinsbons"></SidebarElement>
+        <SidebarElement title="NEWS"></SidebarElement>
+        <SidebarElement title="Corona-Schutzkonzept"></SidebarElement>
+      </div>
+    </div>
     </>
   )
 }
